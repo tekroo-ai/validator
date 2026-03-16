464d6973ec1101e4e97249a6964246600edb733b
paul <paul_coppinger@hotmail.com>
2026-03-16 11:36:05 -0600

    chore: initial scaffold for validator

---
+++ src/main/java/validator/App.java
@@ -0,0 +1,8 @@
+package validator;
+
+/** Entry point. */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from validator!");
+    }
+}
---
+++ src/test/java/validator/AppTest.java
@@ -0,0 +1,11 @@
+package validator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void appInstantiates() {
+        assertTrue(true);
+    }
+}
